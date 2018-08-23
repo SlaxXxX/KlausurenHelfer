49a40fcc6f2e4452114b41e3d13ee7dc5d2416fa
SlaxXxX <Janis.Schneider97@web.de>
2018-08-23 16:53:53 +0200

    added paint `wahlergebnis` example and datastructure `binarytree` example

--- KlausurHelfer.java
+++ KlausurHelfer.java
@@ -3,6 +3,9 @@ package de.dhbwka.java.exercise._vorlagen;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.util.List;
+import java.util.LinkedList;
+
 import javax.swing.*;
 
 @SuppressWarnings("serial")
@@ -25,9 +28,12 @@ public class KlausurHelfer extends JFrame {
 		tab.addTab("LAYOUTS   ", /* KLICK>> */LAYOUTSTAB()/* <<KLICK */);
 		tab.addTab("EVENTS    ", /* KLICK>> */EVENTSTAB()/* <<KLICK */);
 		tab.addTab("THREADS   ", /* KLICK>> */THREADSTAB()/* <<KLICK */);
+		tab.addTab("PAINT     ", /* KLICK>> */PAINTTAB()/* <<KLICK */);
+		tab.addTab("DATASTRUCT", /* KLICK>> */DATASTRUCTTAB()/* <<KLICK */);
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(640, 480);
+		this.setResizable(false);
 		this.setVisible(true);
 	}
 
@@ -256,6 +262,262 @@ public class KlausurHelfer extends JFrame {
 		}).start();
 
 		return threadPanel;
+	}
+
+	// -PAINT->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+	public JPanel PAINTTAB() {
+		final int MARGIN_LEFT = 10;
+		final int MARGIN_RIGHT = 10;
+		final int MARGIN_TOP = 50;
+		Color TOP_COL = new Color(0, 71, 129);
+		Color BG_COL = new Color(121, 170, 211);
+		Color RULER_COL = new Color(62, 133, 192);
+		Font FONT = new Font(Font.SANS_SERIF, Font.BOLD, 16);
+		String INFO_TEXT = "in %";
+
+		final Party[] parties = { new Party(33.0f, "Union", Color.BLACK), new Party(20.5f, "SPD", Color.RED),
+				new Party(12.6f, "AfD", Color.BLUE), new Party(10.7f, "FDP", Color.YELLOW),
+				new Party(9.2f, "Linke", Color.MAGENTA), new Party(8.9f, "Gr\u00FCne", Color.GREEN),
+				new Party(5.1f, "Andere", Color.DARK_GRAY) };
+		double maxPercentageOfParties = 40f;
+
+		JPanel paintPanel = new JPanel() {
+			@Override
+			public void paintComponent(Graphics g) {
+				final int width = this.getWidth();
+				final int height = this.getHeight();
+
+				int maxBarWidth = (width - MARGIN_LEFT - MARGIN_RIGHT) / parties.length;
+				// Max height of bar = 2/3 of component height
+				int totalBarHeight = height * 2 / 3;
+				// height for 1 percent point
+				double heightPerPercent = totalBarHeight / maxPercentageOfParties;
+				// After calculating height for 1 percent, add margin top to
+				// total height
+				totalBarHeight += MARGIN_TOP;
+				g.setFont(FONT);
+				g.setColor(BG_COL); // Background color
+				g.fillRect(0, 0, width, height); // Background rectangle
+				// Top background bar to write title on
+				g.setColor(TOP_COL);
+				g.fillRect(0, 0, width, 30);
+				g.setColor(RULER_COL); // set line/ruler color
+				g.fillRect(0, 30, width, 2); // line below top background bar
+				// Thick line above bottom white bar
+				g.fillRect(0, totalBarHeight, width, 10);
+				// line below bottom white bar
+				g.fillRect(0, totalBarHeight + 40, width, 2);
+				// line below percentage of parties
+				g.fillRect(0, totalBarHeight + 65, width, 2);
+				g.setColor(Color.WHITE);
+				// Bottom white bar
+				g.fillRect(0, totalBarHeight + 10, width, 30);
+				// Top title on the left
+				g.drawString("BUNDESTAGSWAHL 1337", MARGIN_LEFT, 22);
+				// Top info on the right
+				g.drawString(INFO_TEXT, width - 40, 22);
+				// thin white helper lines (10% intervals)
+				for (int i = 1; i <= (int) (maxPercentageOfParties / 10); i++) {
+					g.drawLine(0, (int) (totalBarHeight - i * 10 * heightPerPercent), width,
+							(int) (totalBarHeight - i * 10 * heightPerPercent));
+				}
+				// draw parties with helper function
+				for (int i = 0; i < parties.length; i++) {
+					int partyHeight = (int) (parties[i].getPercentage() * heightPerPercent);
+					this.drawBar(g, MARGIN_LEFT + i * maxBarWidth, totalBarHeight - partyHeight, maxBarWidth,
+							partyHeight, parties[i]);
+				}
+			}
+
+			public void drawBar(Graphics g, int x, int y, int width, int height, Party party) {
+				int margin = 10;
+				int shadow = 2;
+				// draw shadow rectangle
+				g.setColor(java.awt.Color.GRAY);
+				g.fillRect(x + margin + shadow, y - shadow + 1, width - 2 * margin, height);
+				g.setColor(party.getColor()); // draw actual colored bar over
+												// shadow
+				// above line
+				g.fillRect(x + margin, y, width - 2 * margin, height);
+				// below line
+				g.fillRect(x + margin, y + height + 10, width - 2 * margin, 10);
+				g.setColor(java.awt.Color.BLACK);
+				// Draw name of party
+				g.drawString(party.getName(), x + margin, y + height + 36);
+				// Draw percentage value, but replace decimal '.' with ','
+				g.drawString(Float.toString(party.getPercentage()).replace('.', ','), x + margin + 5, y + height + 60);
+			}
+		};
+
+		return paintPanel;
+	}
+
+	private class Party {
+		private float percentage;
+		private String name;
+		private Color color;
+
+		public Party(float percentage, String name, Color color) {
+			this.percentage = percentage;
+			this.name = name;
+			this.color = color;
+		}
+
+		public float getPercentage() {
+			return this.percentage;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public Color getColor() {
+			return this.color;
+		}
+	}
+
+	// -DATASTRUCT->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+	public JPanel DATASTRUCTTAB() {
+		JPanel structPanel = new JPanel();
+		BinaryTree<String> tree = new BinaryTree<>();
+
+		JPanel treePanel = new JPanel() {
+			@Override
+			public void paintComponent(Graphics g) {
+				g.setColor(Color.LIGHT_GRAY);
+				g.fillRect(0, 0, this.getWidth(), this.getHeight());
+				g.setColor(Color.BLACK);
+				paintTree(g, tree, 0, this.getWidth(), 20);
+			}
+
+			private <T extends Comparable<T>> void paintTree(Graphics g, BinaryTree<T> tree, int leftbound,
+					int rightbound, int y) {
+				if (tree.value != null) {
+					g.drawString((String) tree.getValue(), (leftbound + rightbound) / 2, y);
+				}
+				if (tree.left != null) {
+					g.drawLine((leftbound + rightbound) / 2, y, (leftbound * 3 + rightbound) / 4, y + 15);
+					paintTree(g, tree.left, leftbound, (leftbound + rightbound) / 2, y + 30);
+				}
+				if (tree.right != null) {
+					g.drawLine((leftbound + rightbound) / 2, y, (leftbound + rightbound * 3) / 4, y + 15);
+					paintTree(g, tree.right, (leftbound + rightbound) / 2, rightbound, y + 30);
+				}
+			}
+		};
+		treePanel.setPreferredSize(new Dimension(400, 350));
+		treePanel.setBorder(BorderFactory.createLineBorder(Color.black, 3));
+
+		JTextField field = new JTextField();
+		field.setPreferredSize(new Dimension(300, 30));
+		JButton button = new JButton("ADD");
+		button.addActionListener((e) -> {
+			tree.add(field.getText());
+			field.setText("");
+			treePanel.repaint();
+			System.out.println(tree.traverse());
+		});
+
+		structPanel.add(field);
+		structPanel.add(button);
+		structPanel.add(treePanel);
+
+		return structPanel;
+	}
+
+	private class BinaryTree<T extends Comparable<T>> {
+		private T value;
+		private BinaryTree<T> left;
+		private BinaryTree<T> right;
+
+		public BinaryTree() {
+		}
+
+		/**
+		 * Recursive adding of newValue (no duplicates allowed).
+		 *
+		 * @param newValue
+		 *            new value to add
+		 *
+		 * @return <code>true</code> if value has been added, <code>false</code>
+		 *         otherwise
+		 */
+		public boolean add(T newValue) {
+			if (this.value == null) {
+				this.value = newValue;
+			} else if (this.value.compareTo(newValue) == 0) {
+				return false;
+			} else if (this.value.compareTo(newValue) > 0) {
+				return this.getLeft().add(newValue);
+			} else if (this.value.compareTo(newValue) < 0) {
+				return this.getRight().add(newValue);
+			}
+			return true;
+		}
+
+		/**
+		 * Get the node's value
+		 */
+		public T getValue() {
+			return this.value;
+		}
+
+		/**
+		 * Get left node, if none is present (<code>this.left==null</code>)
+		 * create a new node
+		 *
+		 * @return left
+		 */
+		private BinaryTree<T> getLeft() {
+			if (this.left == null) {
+				this.left = new BinaryTree<>();
+			}
+			return this.left;
+		}
+
+		/**
+		 * Get right node, if none is present (<code>this.left==null</code>)
+		 * create a new node
+		 *
+		 * @return right
+		 */
+		private BinaryTree<T> getRight() {
+			if (this.right == null) {
+				this.right = new BinaryTree<>();
+			}
+			return this.right;
+		}
+
+		/**
+		 * Traverse BinaryTree in ascending order and
+		 *
+		 * @return all values in an ordered List<T>
+		 */
+		public List<T> traverse() {
+			List<T> l = new LinkedList<>();
+			this.doTraverse(l);
+			return l;
+		}
+
+		/**
+		 * Traverse BinaryTree in ascending order and add all values to list.
+		 *
+		 * @param list
+		 *            list to add values to
+		 */
+		private void doTraverse(List<T> list) {
+			if (this.left != null) {
+				this.left.doTraverse(list);
+			}
+			if (this.value != null) {
+				list.add(this.value);
+			}
+			if (this.right != null) {
+				this.right.doTraverse(list);
+			}
+		}
 	}
 
 	// -MAIN->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
